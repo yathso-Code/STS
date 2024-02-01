e8557505eb93e7b9c2bb9ad028882f3f0d05f5e0
yatharth <pranjalshukla1617@gmail.com>
2024-02-02 00:01:06 +0530

    navbar close in mobile screen size when we switch one page to other page

--- src/component/reuse/Navbar.js
+++ src/component/reuse/Navbar.js
@@ -7,6 +7,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
   let [newClass, setNewClass]  = useState('');
 
+// =================scroll to show navbar in fiew range=====================
  window.addEventListener('scroll', ()=> {
   let Scrolled = window.scrollY;
   // console.log(Scrolled)
@@ -16,17 +17,26 @@ const Navbar = () => {
     setNewClass('');
 }
  })
+// ==============close the nav bar ===================
+ function closeNav(){
+  document.getElementById('chk-box').checked = false;
+ }
 
     return (
       <nav className={newClass}>
         <input type="checkbox" id="chk-box" />
         <div className="logo">STS Education India</div>
-        <div className="links">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/course">Courses</NavLink>
-        <NavLink to="/onlineLearn">Online Learning</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+        <div className="links" id='links'>
+        <NavLink to="/" onClick={closeNav} >Home</NavLink>
+
+        <NavLink to="/about" onClick={closeNav} >About</NavLink>
+
+        <NavLink to="/course" onClick={closeNav} >Courses</NavLink>
+
+        <NavLink to="/onlineLearn"  onClick={closeNav}>Online Learning</NavLink>
+
+        <NavLink to="/contact"  onClick={closeNav} >Contact</NavLink>
+          
         </div>
         <label htmlFor="chk-box"><FontAwesomeIcon icon={faBars} /></label>
       </nav>
