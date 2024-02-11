79776f758531723aee57516cca01fe633f8cf4ff
yatharth <pranjalshukla1617@gmail.com>
2024-02-11 21:57:12 +0530

    add try again page in 404 page

--- src/component/Error.js
+++ src/component/Error.js
@@ -1,11 +1,17 @@
+import { render } from '@testing-library/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
+  
   return (
-    <div>
+    <>
+      <Link to="/"  style={{position: 'absolute', top: '50%', left: '50%',
+           transform: 'translate(-50%, -50%)', color: 'white', fontSize: '40px', fontWeight: 'bold'}}
+      > Try again </Link>
       <img src='https://images.unsplash.com/photo-1623018035782-b269248df916?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXJyb3J8ZW58MHx8MHx8fDA%3D'
       style={{width: '100vw', height: '100vh'}}/>
-    </div>
+    </>
   )
 }
 
