7d8c90ce9d3d5a9e9b27c441b69407041c8074b5
Scar <kabir708455@gmail.com>
2024-02-12 22:46:50 +0530

    changed Flex Card

--- src/component/reuse/FlexBoxCard.js
+++ src/component/reuse/FlexBoxCard.js
@@ -1,38 +1,38 @@
 import React from 'react'
 import './FlexBoxCard.css'
 
-const FlexBoxCard = ( ) => {
+const FlexBoxCard = () => {
 
     let courses = [
         {
             name: 'Front-End Developer',
             sub: 'HTML5 + CSS + JS Advanced + Bootstrap5 + React-Js + Next-Js + Git/Github',
-            img: 'https://t3.ftcdn.net/jpg/02/92/88/72/360_F_292887204_2wH041phSQo70eqaE9GRqFvn5MmQ4B8w.jpg'
+            img: 'https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?size=626&ext=jpg&uid=R120958505&ga=GA1.1.543567340.1680985813&semt=ais'
         },
         {
             name: 'Back-end Developer',
             sub: 'SQL + MySQL + MongoDB + Node-Js + Express-Js + Git/Github',
-            img: 'https://t3.ftcdn.net/jpg/02/92/88/72/360_F_292887204_2wH041phSQo70eqaE9GRqFvn5MmQ4B8w.jpg'
+            img: 'https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?size=626&ext=jpg&uid=R120958505&ga=GA1.1.543567340.1680985813&semt=ais'
         },
         {
             name: 'Full-Stack Developer',
             sub: 'HTML5 + CSS + JS-Advanced ES6 + Bootstrap5 + React-Js + Next-Js + Git/Github + SQL + MySQL + MongoDB + Node-Js + Express-Js',
-            img: 'https://t3.ftcdn.net/jpg/02/92/88/72/360_F_292887204_2wH041phSQo70eqaE9GRqFvn5MmQ4B8w.jpg'
+            img: 'https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?size=626&ext=jpg&uid=R120958505&ga=GA1.1.543567340.1680985813&semt=ais'
         },
         {
             name: 'PHP Developer',
             sub: 'HTML5 + CSS + JS-Advanced ES6 + Bootstrap5 + React-Js + Next-Js + Git + Github',
-            img: 'https://t3.ftcdn.net/jpg/02/92/88/72/360_F_292887204_2wH041phSQo70eqaE9GRqFvn5MmQ4B8w.jpg'
+            img: 'https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?size=626&ext=jpg&uid=R120958505&ga=GA1.1.543567340.1680985813&semt=ais'
         },
         {
             name: 'Database Management',
             sub: 'SQL, MySQL, MongoDB',
-            img: 'https://t3.ftcdn.net/jpg/02/92/88/72/360_F_292887204_2wH041phSQo70eqaE9GRqFvn5MmQ4B8w.jpg'
+            img: 'https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?size=626&ext=jpg&uid=R120958505&ga=GA1.1.543567340.1680985813&semt=ais'
         },
         {
             name: 'Programming Languages',
-            sub: 'C/C++ + Python + Java',
-            img: 'https://t3.ftcdn.net/jpg/02/92/88/72/360_F_292887204_2wH041phSQo70eqaE9GRqFvn5MmQ4B8w.jpg'
+            sub: 'Java + Python + C/C++',
+            img: 'https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?size=626&ext=jpg&uid=R120958505&ga=GA1.1.543567340.1680985813&semt=ais'
         },
     ];
 
@@ -42,17 +42,16 @@ const FlexBoxCard = ( ) => {
                 {
                     courses.map((item) => (
                         <div class="card">
-                            <img src={item.img} alt="" />
-                            <div class="card__content">
-                                <p class="card__title">{item.name}</p>
-                                <p class="card__description">{item.sub}</p>
+                            <div class="image">
+                                <img src={item.img} alt="" />
+                            </div>
+                            <div class="card-content">
+                                <h1 class="card-title">{item.name}</h1>
+                                <p class="card-text">{item.sub}</p>
                             </div>
                         </div>
                     ))
                 }
-
-
-
             </div>
         </>
     )
